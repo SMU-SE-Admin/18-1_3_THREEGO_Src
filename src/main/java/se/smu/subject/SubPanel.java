17516412abe4fb848f114ee23716a8de8edff18b
201411205 <bibiq5045@gmail.com>
2018-05-21 18:43:56 +0900

    #1164 과목 패키지 추가
    과목 패키지 추가

---
+++ src/main/java/se/smu/subject/SubPanel.java
@@ -0,0 +1,159 @@
+package se.smu.subject;
+
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableRowSorter;
+
+import se.smu.db.DBConnection;
+import se.smu.todolist.TodoEnroll;
+
+public class SubPanel extends JPanel implements ActionListener{
+	private String subTitle[] = { "년도-학기", "과목명", "요일", "시간", "교수", "변경", "삭제" };
+	private DefaultTableModel subtableModel;
+	private JTable table;
+	private JLabel lblId;
+	private JScrollPane sp;
+	private JPanel panel;
+	private JButton enrollButton;
+	private String id;
+	public Object input_data[] = new Object[7];
+	
+	public SubPanel(String id) {
+		setLayout(null);
+		setSize(500, 400);
+		subtableModel = new DefaultTableModel(subTitle, 0);
+		table = new JTable(subtableModel);
+		
+		refreshTable(id, subtableModel, subTitle);
+
+		sp = new JScrollPane(table);
+		sp.setBounds(0, 47, 500, 350);
+		add(sp);
+
+		enrollButton = new JButton("등록");
+		enrollButton.setBounds(415, 8, 71, 33);
+		add(enrollButton);
+
+		enrollButton.addActionListener(this);
+
+		JLabel lblId_1 = new JLabel("ID : " + id);
+		lblId_1.setBounds(14, 17, 121, 18);
+		add(lblId_1);
+		
+		table.getColumnModel().getColumn(5).setCellEditor(new SubTableCell("변경", id, table));
+		table.getColumnModel().getColumn(5).setCellRenderer(new SubTableCell("변경", id, table));
+		table.getColumnModel().getColumn(6).setCellEditor(new SubTableCell("삭제", id, table));
+		table.getColumnModel().getColumn(6).setCellRenderer(new SubTableCell("삭제", id, table));
+		
+		this.id = id;
+		
+		sort();
+		
+		this.setVisible(true);
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == enrollButton) {
+			Frame fs = new enrollWindow(subtableModel, input_data, id);
+			fs.setVisible(true);
+		} 
+	}
+	
+	private void sort() {
+		TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<DefaultTableModel>(subtableModel);
+		table.setRowSorter(sorter);
+	}
+	
+	private void refreshTable(String id, DefaultTableModel tm, Object[] headers) {
+		Vector<Object> columns = new Vector<Object>();
+		for(int i=0; i<headers.length; i++) {
+			columns.add(headers[i]);
+		}
+		DBConnection db = new DBConnection();
+		Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+		data = db.getSubject(id);
+		tm.setDataVector(data, columns);
+		db.close();
+	}
+}
+
+class SubTableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer {
+	private JButton btn;
+	private String id;
+	private JTable tbl;
+	
+	public SubTableCell(final String label, String _id, final JTable table) {
+		btn = new JButton(label);
+		id = _id;
+		tbl = table;
+		btn.setText(label);
+				
+		btn.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				String type = btn.getText();
+				int row = table.getSelectedRow();
+				if ("변경".equals(type)) {
+					Object input_data[] = new Object[7];
+					input_data = getRows(table, row);
+					Frame fr = new enrollModifyWindow((DefaultTableModel) table.getModel(), input_data, row, id);
+					fr.setVisible(true);
+				}else if("삭제".equals(type)) {
+					if(JOptionPane.showConfirmDialog(null, "해당 과목을 삭제하시곘습니까?", "삭제", 
+							JOptionPane.YES_NO_OPTION , JOptionPane.WARNING_MESSAGE) == JOptionPane.YES_OPTION) {
+						DBConnection db = new DBConnection();
+						if(db.deleteSubject(id, getRows(table, row))) {
+							db.close();
+							int tmRow = table.convertRowIndexToModel(row);
+							DefaultTableModel tm = (DefaultTableModel) table.getModel();
+							tm.removeRow(tmRow);
+						}
+					}
+				}
+			}
+		});
+		
+		
+	}
+
+	public Object getCellEditorValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		// TODO Auto-generated method stub
+		return btn;
+	}
+
+	public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+		return btn;
+	}
+	
+	private Object[] getRows(JTable tbl, int row) {
+		Object rows[] = new Object[7];
+		for (int i = 0; i < tbl.getColumnCount(); i++) {
+			Object obj = tbl.getValueAt(row, i);
+			rows[i] = obj;
+		}
+		return rows;
+	}
+}
---
+++ src/main/java/se/smu/subject/enrollWindow.java
@@ -0,0 +1,195 @@
+package se.smu.subject;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.ButtonModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import se.smu.db.DBConnection;
+
+public class enrollWindow extends JFrame {
+	JTextField text_Sem;
+	JTextField text_Subname;
+	JTextField text_Profname;
+	String[] starthour = {"09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};		
+	String[] endhour = {"09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
+	JButton completeButton;
+	JLabel sem_label;
+	JLabel subname_label;
+	JLabel today_label;
+	JLabel time_label;
+	JLabel profname_label;
+	JRadioButton rb1;
+	JRadioButton rb2;
+	JRadioButton rb3;
+	JRadioButton rb4;
+	JRadioButton rb5;
+	JRadioButton rb6;
+	JRadioButton rb7;
+	JComboBox start_comboBox;
+	JComboBox end_comboBox;
+	ButtonGroup grouprb;
+	
+	public enrollWindow(final DefaultTableModel subtableModel, final Object input_data[], final String id) {
+		setLocation(300, 300);
+		setSize(520,400);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID : " + id);
+		lblNewLabel.setBounds(28, 16, 117, 18);
+		getContentPane().add(lblNewLabel);
+		
+		completeButton = new JButton("완료");
+		completeButton.setBounds(384, 12, 105, 27);
+		getContentPane().add(completeButton);
+		
+		sem_label = new JLabel("년도-학기");
+		sem_label.setBounds(44, 90, 87, 27);
+		getContentPane().add(sem_label);
+		
+		subname_label = new JLabel("과목명");
+		subname_label.setBounds(44, 130, 87, 27);
+		getContentPane().add(subname_label);
+		
+		today_label = new JLabel("요일");
+		today_label.setBounds(42, 169, 87, 27);
+		getContentPane().add(today_label);
+		
+		time_label = new JLabel("시간");
+		time_label.setBounds(42, 213, 87, 27);
+		getContentPane().add(time_label);
+		
+		profname_label = new JLabel("교수명");
+		profname_label.setBounds(42, 252, 87, 27);
+		getContentPane().add(profname_label);
+		
+		text_Sem = new JTextField();
+		text_Sem.setBounds(145, 91, 214, 27);
+		getContentPane().add(text_Sem);
+		text_Sem.setColumns(10);
+		
+		text_Subname = new JTextField();
+		text_Subname.setBounds(145, 131, 214, 26);
+		getContentPane().add(text_Subname);
+		text_Subname.setColumns(10);
+		
+		text_Profname = new JTextField();
+		text_Profname.setColumns(10);
+		text_Profname.setBounds(145, 252, 214, 26);
+		getContentPane().add(text_Profname);
+		
+		rb1 = new JRadioButton("월", true);
+		rb2 = new JRadioButton("화");
+		rb3 = new JRadioButton("수");
+		rb4 = new JRadioButton("목");
+		rb5 = new JRadioButton("금");
+		rb6 = new JRadioButton("토");
+		rb7 = new JRadioButton("일");
+		
+		grouprb = new ButtonGroup();
+		
+		grouprb.add(rb1);
+		rb1.setBounds(139, 172, 50, 20);
+		grouprb.add(rb2);
+		rb2.setBounds(189, 172, 50, 20);
+		grouprb.add(rb3);
+		rb3.setBounds(239, 172, 50, 20);
+		grouprb.add(rb4);
+		rb4.setBounds(289, 172, 50, 20);
+		grouprb.add(rb5);
+		rb5.setBounds(339, 172, 50, 20);
+		grouprb.add(rb6);
+		rb6.setBounds(389, 172, 50, 20);
+		grouprb.add(rb7);
+		rb7.setBounds(439, 172, 50, 20);
+	
+		getContentPane().add(rb1);
+		getContentPane().add(rb2);
+		getContentPane().add(rb3);
+		getContentPane().add(rb4);
+		getContentPane().add(rb5);
+		getContentPane().add(rb6);
+		getContentPane().add(rb7);
+		
+		start_comboBox = new JComboBox(starthour);
+		start_comboBox.setBounds(156, 214, 67, 24);
+		getContentPane().add(start_comboBox);
+		
+		end_comboBox = new JComboBox(endhour);
+		end_comboBox.setBounds(289, 214, 65, 24);
+		getContentPane().add(end_comboBox);
+		
+		JLabel lblNewLabel_2 = new JLabel("   ~");
+		lblNewLabel_2.setBounds(239, 217, 36, 18);
+		getContentPane().add(lblNewLabel_2);
+
+		completeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!text_Sem.getText().equals("") && !text_Subname.getText().equals("") && !text_Profname.getText().equals("")) {
+					input_data[0] = text_Sem.getText();
+					input_data[1] = text_Subname.getText();
+					input_data[2] = selectedRadioContents(grouprb);
+					input_data[3] = start_comboBox.getSelectedItem() + " ~ " + end_comboBox.getSelectedItem();
+					input_data[4] = text_Profname.getText();
+					input_data[5] = "변경";
+					input_data[6] = "삭제";
+					if(!checkDupl(subtableModel, input_data)) {
+						DBConnection db = new DBConnection();
+						db.setSubject(id, input_data);
+						db.close();
+						subtableModel.addRow(input_data);
+					}else {
+						JOptionPane.showMessageDialog(null, "중복된 과목이 있습니다!", "ERROR", JOptionPane.ERROR_MESSAGE);
+					}
+					setVisible(false);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "누락된 곳이 있습니다!", "ERROR", JOptionPane.ERROR_MESSAGE);
+				}
+				
+			}
+		});
+		
+	}
+	public String selectedRadioContents(ButtonGroup gr) {
+		Enumeration<AbstractButton> enums = gr.getElements();
+		String contents = "";
+		while(enums.hasMoreElements()) {            
+		    AbstractButton ab = enums.nextElement();    
+		    JRadioButton jb = (JRadioButton)ab;          
+		    if(jb.isSelected())                         
+		        contents = jb.getText();
+		}
+		return contents;
+	}
+	
+	private boolean checkDupl(DefaultTableModel tm, Object[] row) {
+		Vector data = tm.getDataVector();
+		for(int i=0; i<data.size(); i++) {
+			Vector tmp = (Vector) data.get(i);
+			int count = 0;
+			for(int j=0; j<5; j++) {
+				if(tmp.get(j).equals(row[j])){
+					count++;
+				}
+			}
+			if(count == 5)
+				return true;
+			
+		}
+		return false;
+	}
+	
+}
